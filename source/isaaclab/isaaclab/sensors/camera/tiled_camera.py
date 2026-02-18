d484f4dd155e6b1550dfb94341a30704ee07b205
Nathan Cournia <ncournia@nvidia.com>
2026-02-18 13:39:59 -0800

    Disable image output.

--- source/isaaclab/isaaclab/sensors/camera/tiled_camera.py
+++ source/isaaclab/isaaclab/sensors/camera/tiled_camera.py
@@ -177,7 +177,7 @@ class TiledCamera(Camera):
                 width=self.cfg.width, height=self.cfg.height, num_cameras=self._view.count, num_envs=self._num_envs,
                 data_types=self.cfg.data_types
                 #, simple_shading_mode=False
-                , image_folder="/tmp/ovrtx"
+                #, image_folder="/tmp/ovrtx"
                 , use_ovrtx_cloning=False  # Default: use OVRTX internal cloning for faster initialization
             )
             # Lazy-load the renderer class
